005adbca6e5a1d9f4f9f676e00a84f791f4d7bec
Miilaaaa <milena.schuerch@gmail.com>
2025-04-12 10:18:28 +0200

    Updates

--- src/routes/film.ts
+++ src/routes/film.ts
@@ -1,16 +1,21 @@
 import {Request, Response, Router} from 'express';
-import {db} from '../db';
-import {addFilmtoCategory} from "../services/filmService";
+import {addFilmtoCategory, getAllFilms, getFilmById, postNewFilm, updateFilm, deleteFilm, deleteFilmFromCategory  }
+    from "../services/filmService";
 
-const filmRouter = Router();
-
+const filmRouter:Router = Router();     //erzeugt einen neuen Router
 
 /**
  * @swagger
  * /film:
  *   get:
- *     summary: Retrieve a list of all films
+ *     summary: Retrieve a list of all films (optionally filtered by title)
  *     tags: [film]
+ *     parameters:
+ *       - in: query
+ *         name: title
+ *         schema:
+ *           type: string
+ *         description: Filter films by title (optional)
  *     responses:
  *       200:
  *         description: A list of films
@@ -27,49 +32,81 @@ const filmRouter = Router();
  *                     type: string
  *                   description:
  *                     type: string
- *       404:
+ *       500:
  *         description: Failed to load films
  */
 
-// GetAll – Liste aller Filme abrufen
-filmRouter.get('/', async (_req: Request, res: Response) => {
-    const connection = db();
+//GetAll: Liste aller Filme abrufen mit optionalem Titelfilter
+filmRouter.get('/', async (req: Request, res: Response) => {
     try {
-        const films = await connection.select("*").from("film");
-        res.send(films);
+        const films = await getAllFilms(req.query.title as string);
+        res.send(films);        // sendet die Filme zurück als JSON
     } catch (error) {
-        console.error("Error retrieving films from film: ", error);
-        res.status(404).send({ error: 'Failed to load films' });
+        console.error("Error retrieving films:", error);        //falls Datenbank nicht erreichbar
+        res.status(500).send({ error: "Failed to load films" });
     }
 });
 
 
 
-//GetByID einzelnes Element nach ID abrufen
+
+/**
+ * @swagger
+ * /film/{id}:
+ *   get:
+ *     summary: Returns a film by its ID
+ *     tags: [film]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the film to retrieve
+ *     responses:
+ *       200:
+ *         description: Film successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 film_id:
+ *                   type: integer
+ *                 title:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *       404:
+ *         description: Film not found
+ *       500:
+ *         description: Internal server error
+ */
+
+
+// GetByID – einzelnes Element nach ID abrufen
 filmRouter.get('/:id', async (req: Request, res: Response) => {
-    const connection = db();
-    const film = await connection.select("*")
-        .from("film")
-        .where("film_id", req.params.id)
-        .first();
+    try {
+        const film = await getFilmById(Number(req.params.id));
 
-    console.log("Selected film: ", film);
-
-    if (!film) {
-        res.status(404).send({error: "Film not found"});
-        return
+        if (!film) {
+            res.status(404).send({error: "Film not found"});        //wenn der Film nicht existiert
+            return;
+        }
+        res.send(film);
+    } catch (error) {
+        console.error("Error retrieving film by ID: ", error);
+        res.status(500).send({error: "Internal server error"});
     }
-
-    res.send(film);
 });
 
 
-//Insert: Post neues Element erstellen
+
 /**
  * @swagger
  * /film:
  *   post:
- *     summary: Neuen Film erstellen
+ *     summary: Create a new film
  *     tags: [film]
  *     requestBody:
  *       required: true
@@ -115,7 +152,7 @@ filmRouter.get('/:id', async (req: Request, res: Response) => {
  *                 example: Deleted Scenes,Behind the Scenes
  *     responses:
  *       200:
- *         description: Film erfolgreich erstellt
+ *         description: Film successfully created
  *         content:
  *           application/json:
  *             schema:
@@ -123,58 +160,209 @@ filmRouter.get('/:id', async (req: Request, res: Response) => {
  *               properties:
  *                 id:
  *                   type: integer
- *                   description: ID des neu erstellten Films
- *       400:
- *         description: Ungültige Eingabe
+ *                   description: ID of the newly created film
+ *       500:
+ *         description: Internal server error while creating film
  */
 
 
-
+//Insert: Post, neues Element erstellen
 filmRouter.post('/', async (req: Request, res: Response) => {
     console.log("Creating film: ", req.body);
 
-    const connection = db();
-    const insertOperation = await connection.insert(req.body).into("film");
-
-    res.send({id: insertOperation[0]});
+    try {
+        const filmId = await postNewFilm(req.body);     //erstellt einen neuen Film aus dem Body
+        res.send({ id: filmId });
+    } catch (error) {
+        console.error("Error inserting film into film: ", error);
+        res.status(500).send({ error: 'Failed to create film' }); //Meldung 500 passender als 404 das um Serverfehler beim Ein
+    }
 });
+
+
+
+/**
+ * @swagger
+ * /film/{id}:
+ *   put:
+ *     summary: Update a film by ID
+ *     tags: [film]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the film to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - description
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: The Matrix Reloaded
+ *               description:
+ *                 type: string
+ *                 example: Neo continues his fight against the machines.
+ *               release_year:
+ *                 type: string
+ *                 example: 2003
+ *               language_id:
+ *                 type: integer
+ *                 example: 1
+ *               original_language_id:
+ *                 type: integer
+ *                 example: 2
+ *               rental_duration:
+ *                 type: integer
+ *                 example: 7
+ *               rental_rate:
+ *                 type: number
+ *                 format: float
+ *                 example: 4.99
+ *               length:
+ *                 type: integer
+ *                 example: 138
+ *               replacement_cost:
+ *                 type: number
+ *                 format: float
+ *                 example: 19.99
+ *               rating:
+ *                 type: string
+ *                 example: PG-13
+ *               special_features:
+ *                 type: string
+ *                 example: Deleted Scenes,Behind the Scenes
+ *     responses:
+ *       200:
+ *         description: Film successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: Updated 1 film(s)
+ *       404:
+ *         description: Film not found
+ *       500:
+ *         description: Internal server error
+ */
+
 
 //Update vorhandenes Element aktualisieren
 filmRouter.put('/:id', async (req: Request, res: Response) => {
-    const connection = db();
+    try {
+        const updateCount = await updateFilm(Number(req.params.id), req.body);
 
-    const film = await connection.select("*")
-        .from("film")
-        .where("film_id", req.params.id).first();
+        if (!updateCount) {
+            res.status(404).send({ error: "Film not found" });
+            return;
+        }
 
-    if (!film) {
-        res.status(404).send({error: "Film not found"});
-        return
+        res.send(`Updated ${updateCount} film(s)`);
+    } catch (error) {
+        console.error("Error updating film: ", error);
+        res.status(500).send({ error: "Failed to update film" });
     }
-
-    film.title = req.body.title;
-    film.description = req.body.description;
-
-    const updateOperation = await connection("film").update(film)
-        .where("film_id", req.params.id);
-    res.send(`Updated ${updateOperation} films`);
 });
 
-// delete film ID, Element löschen
+
+/**
+ * @swagger
+ * /film/{id}:
+ *   delete:
+ *     summary: Delete a film by its ID
+ *     tags: [film]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the film to delete
+ *     responses:
+ *       200:
+ *         description: Film successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: Deleted 1 film(s)
+ *       404:
+ *         description: Film not found
+ *       500:
+ *         description: Internal server error
+ */
+
+//delete: Element löschen
 filmRouter.delete('/:id', async (req: Request, res: Response) => {
-    const connection = db();
-    const deleteOperation = await connection("film")
-        .where("film_id", req.params.id).delete();
+    try {
+        const deleteCount = await deleteFilm(Number(req.params.id));
 
-    if (!deleteOperation) {
-        res.status(404).send({error: "Film not found"});
-        return
+        if (!deleteCount) {
+            res.status(404).send({error: "Film not found"});
+            return;
+        }
+
+        res.send(`Deleted ${deleteCount} films`);
+    } catch (error) {
+        console.error("Error deleting film: ", error);
+        res.status(500).send({ error: "Failed to delete film" });
     }
-
-    res.send(`Deleted ${deleteOperation} films`);
 });
 
-filmRouter.post('/:film_id/category/:category_id', async (req: Request, res: Response) => {
+
+/**
+ * @swagger
+ * /film/{film_id}/category/{category_id}:
+ *   post:
+ *     summary: Add a film to a category
+ *     tags: [film]
+ *     parameters:
+ *       - in: path
+ *         name: film_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the film to add to the category
+ *       - in: path
+ *         name: category_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the category to add the film to
+ *     responses:
+ *       201:
+ *         description: Film successfully added to the category
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Film added to category"
+ *                 filmId:
+ *                   type: integer
+ *                   example: 1
+ *                 categoryId:
+ *                   type: integer
+ *                   example: 2
+ *       400:
+ *         description: Failed to add film to category
+ *       404:
+ *         description: Film or category not found
+ *       500:
+ *         description: Internal server error
+ */
+
+//post: Film einer Kategorie zuordnen
+    filmRouter.post('/:film_id/category/:category_id', async (req: Request, res: Response) => {
     const categoryId = req.params.category_id;
     const filmId = req.params.film_id;
 
@@ -182,12 +370,77 @@ filmRouter.post('/:film_id/category/:category_id', async (req: Request, res: Res
     try {
         await addFilmtoCategory(Number(filmId), Number(categoryId));
         console.log(`Film ${filmId} added to category ${categoryId}`);
-
-        res.status(201).send(" Film- Category created");
+        res.status(201).send({message: " Film added to category", filmId, categoryId});
     } catch (error) {
-        console.error("Error adding actor to film: ", error);
-        res.status(400).send({error: "Failed to add actor to film. " + (error)});
+        console.error("Error adding film to category: ", error);
+        res.status(400).send({error: "Failed to add film to category. " + error});
     }
-})
+});
+
+
+/**
+ * @swagger
+ * /film/{film_id}/category/{category_id}:
+ *   delete:
+ *     summary: Remove a film from a category
+ *     tags: [film]
+ *     parameters:
+ *       - in: path
+ *         name: film_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the film to remove from the category
+ *       - in: path
+ *         name: category_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the category to remove the film from
+ *     responses:
+ *       200:
+ *         description: Film successfully removed from the category
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Film removed from category"
+ *                 filmId:
+ *                   type: integer
+ *                   example: 1
+ *                 categoryId:
+ *                   type: integer
+ *                   example: 2
+ *       400:
+ *         description: Failed to remove film from category
+ *       404:
+ *         description: Film or category not found
+ *       500:
+ *         description: Internal server error
+ */
+
+//Delete: Verknüpfung Film- Category entfernen
+filmRouter.delete('/:film_id/category/:category_id', async (req: Request, res: Response) => {
+    const categoryId = req.params.category_id;
+    const filmId = req.params.film_id;
+
+    try {
+        const deleteCount = await deleteFilmFromCategory(Number(filmId), Number(categoryId));
+
+        if (!deleteCount) {
+            res.status(404).send({ error: "Film or Category not found" });
+            return;
+        }
+
+        res.send(`Removed ${deleteCount} film(s) from category`);
+    } catch (error) {
+        console.error("Error removing film from category: ", error);
+        res.status(500).send({ error: "Failed to remove film from category" });
+    }
+});
+
 
 export default filmRouter;
--- src/services/filmService.ts
+++ src/services/filmService.ts
@@ -1,6 +1,147 @@
 import {db} from "../db";
 
+/**
+ * Ruft alle Filme ab. Optional kann nach dem Filmtitel gefiltert werden.
+ *
+ * @param {string} [title] - optionaler Titel-Filter (beginnt mit dem gegebenen Titel)
+ * @returns {Promise<any[]>} - Eine Liste aller Filme, gefiltert oder vollständig.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export async function getAllFilms(title?: string) {
+    const connection = db();
 
+    if (title) {
+        return connection
+            .select("*")
+            .from("film")
+            .whereLike("title", `${title}%`);
+    } else {
+        return connection
+        .select("*")
+        .from("film");
+    }
+}
+
+/**
+ * Ruft einen Film anhand seiner ID ab.
+ *
+ * @param {number} id - Die ID des Films.
+ * @returns {Promise<any>} - Das Filmobjekt oder undefined, wenn nicht gefunden.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export async function getFilmById(id: number) {
+    const connection = db();
+    const film = await connection
+        .select("*")
+        .from("film")
+        .where("film_id", id)
+        .first();
+
+    return film;
+}
+
+/**
+ * Fügt einen neuen Film in die Datenbank ein.
+ *
+ * @param {any} filmData - Die Filmdaten.
+ * @returns {Promise<number>} - Die ID des neu eingefügten Films.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export const postNewFilm = async (filmData: any): Promise<number> => {
+    const connection = db();
+    const insertOperation = await connection.insert(filmData).into("film");
+    return insertOperation[0];
+};
+
+/**
+ * Aktualisiert einen bestehenden Film mit neuen Daten.
+ *
+ * @param {number} id - Die ID des Films, der aktualisiert werden soll.
+ * @param {object} updateData - Die neuen Filmdaten.
+ * @param {string} updateData.title - Titel des Films.
+ * @param {string} updateData.description - Beschreibung des Films.
+ * @param {string} [updateData.release_year] - Erscheinungsjahr.
+ * @param {number} [updateData.language_id] - Sprache.
+ * @param {number} [updateData.original_language_id] - Originalsprache.
+ * @param {number} [updateData.rental_duration] - Mietdauer.
+ * @param {number} [updateData.rental_rate] - Mietpreis.
+ * @param {number} [updateData.length] - Länge des Films.
+ * @param {number} [updateData.replacement_cost] - Ersatzkosten.
+ * @param {string} [updateData.rating] - Bewertung.
+ * @param {string} [updateData.special_features] - Spezialfunktionen.
+ * @returns {Promise<number | null>} - Anzahl aktualisierter Datensätze oder null, wenn der Film nicht gefunden wurde.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export async function updateFilm(
+    id: number,
+    updateData: {
+        title: string,
+        description: string,
+        release_year?: string,
+        language_id?: number,
+        original_language_id?: number,
+        rental_duration?: number,
+        rental_rate?: number,
+        length?: number,
+        replacement_cost?: number,
+        rating?: string,
+        special_features?: string
+    }
+) {
+    const connection = db();
+
+    const film = await connection("film")
+        .where("film_id", id)
+        .first();
+
+    if (!film) {
+        return null;
+    }
+
+    const updateOperation = await connection("film")
+        .where("film_id", id)
+        .update({
+            title: updateData.title,
+            description: updateData.description,
+            release_year: updateData.release_year,
+            language_id: updateData.language_id,
+            original_language_id: updateData.original_language_id,
+            rental_duration: updateData.rental_duration,
+            rental_rate: updateData.rental_rate,
+            length: updateData.length,
+            replacement_cost: updateData.replacement_cost,
+            rating: updateData.rating,
+            special_features: updateData.special_features,
+        });
+
+    return updateOperation;
+}
+
+
+
+/**
+ * Löscht einen Film anhand seiner ID.
+ *
+ * @param {number} id - Die ID des zu löschenden Films.
+ * @returns {Promise<number>} - Anzahl der gelöschten Datensätze (0 oder 1).
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export async function deleteFilm(id: number): Promise<number> {
+    const connection = db();
+    const deleteCount = await connection("film")
+        .where("film_id", id)
+        .delete();
+    return deleteCount;
+}
+
+/**
+ * Verknüpft einen Film mit einer Kategorie.
+ *
+ * @param {number} categoryId - Die ID der Kategorie.
+ * @param {number} filmId - Die ID des Films.
+ * @returns {Promise<any>} - Das Ergebnis der Insert-Operation.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
 export async function addFilmtoCategory(categoryId: number, filmId: number) {
     const connection = db();
     const insertOperation = await connection("film_category")
@@ -12,4 +153,23 @@ export async function addFilmtoCategory(categoryId: number, filmId: number) {
     console.log("Inserted film to category: ", insertOperation);
 
     return insertOperation;
+}
+
+/**
+ * Entfernt die Verknüpfung eines Films mit einer Kategorie.
+ *
+ * @param {number} categoryId - Die ID der Kategorie.
+ * @param {number} filmId - Die ID des Films.
+ * @returns {Promise<number>} - Anzahl der gelöschten Verknüpfungen.
+ * @throws {Error} - Bei Datenbankfehlern.
+ */
+export async function deleteFilmFromCategory(categoryId: number, filmId: number) {
+    const connection = db();
+    const deleteOperation = await connection("film_category")
+        .where("category_id", categoryId)
+        .andWhere("film_id", filmId)
+        .delete();
+
+    console.log("Deleted film form category: ", deleteOperation);
+    return deleteOperation;
 }
