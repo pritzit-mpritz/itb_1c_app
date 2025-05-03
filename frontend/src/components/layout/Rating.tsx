daede40a9d8c623a928c0d839fdf5312a3db25dc
Miilaaaa <milena.schuerch@gmail.com>
2025-05-03 15:22:00 +0200

    Updates

--- frontend/src/components/layout/Rating.tsx
+++ frontend/src/components/layout/Rating.tsx
@@ -2,34 +2,33 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
 
 export default function BasicRating() {
     const [value, setValue] = React.useState<number | null>(2);
 
     return (
         <Box sx={{ '& > legend': { mt: 2 } }}>
-            <Typography component="legend">Controlled</Typography>
+            <Typography component="legend">Over all</Typography>
             <Rating
-                name="simple-controlled"
+                name="half-rating" defaultValue={2.5} precision={0.5}
                 value={value}
-                onChange={(event, newValue) => {
+                getLabelText={(value: number) => `${value} Heart${value !== 1 ? 's' : ''}`}
+                icon={<FavoriteIcon sx={{ color: 'blueviolet '}} fontSize="inherit" />}
+                emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+                onChange={(_event, newValue) => {
                     setValue(newValue);
                 }}
             />
-            <Typography component="legend">Uncontrolled</Typography>
-            <Rating
-                name="simple-uncontrolled"
-                onChange={(event, newValue) => {
-                    console.log(newValue);
-                }}
-                defaultValue={2}
-            />
-            <Typography component="legend">Read only</Typography>
-            <Rating name="read-only" value={value} readOnly />
-            <Typography component="legend">Disabled</Typography>
-            <Rating name="disabled" value={value} disabled />
-            <Typography component="legend">No rating given</Typography>
-            <Rating name="no-value" value={null} />
+            <Typography component="legend">Music</Typography>
+            <Rating name="half-rating" value={value} />
+            <Typography component="legend">Actors</Typography>
+            <Rating name="half-rating" value={value} />
+
+
+
         </Box>
     );
 }
--- frontend/src/pages/FilmPage.tsx
+++ frontend/src/pages/FilmPage.tsx
@@ -1,10 +1,12 @@
 import ListDividers from '../components/layout/List.tsx';
+import BasicRating from "../components/layout/Rating.tsx";
 
 
 const FilmPage = () => {
     return (
         <div>
             <ListDividers />
+            <BasicRating />
 
         </div>
     );
