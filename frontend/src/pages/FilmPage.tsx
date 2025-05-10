f8ff6e4a7a46b94cdc299a8709dc59dc99ec9a17
Leon.R <leonreiter@glattnet.ch>
2025-05-10 11:40:03 +0200

    erste dokumentation

--- frontend/src/pages/FilmPage.tsx
+++ frontend/src/pages/FilmPage.tsx
@@ -6,14 +6,31 @@ import Button from "@mui/material/Button";
 import JsonView from "@uiw/react-json-view";
 
 export interface InputType {
+    film_id: string;
     title: string;
+    description: string;
+    release_year: string;
+    rental_duration: string;
+    rental_rate: string;
     length: string;
+    replacement_cost: string;
+    rating: string;
+    special_features: string;
 }
 
 const defaultInput: InputType = {
+    film_id: "",
     title: "",
-    length: "0"
+    description: "",
+    release_year: "",
+    rental_duration: "3 Wochen",
+    rental_rate: "5.69 CHF",
+    length: "",
+    replacement_cost: "70 CHF",
+    rating: "",
+    special_features: "",
 }
+
 
 const FilmPage = () => {
     const [input, setInput] = React.useState<InputType>(defaultInput)
@@ -38,9 +55,16 @@ const FilmPage = () => {
 
     return (
         <div>
-            Film Page
+            FilmPage
             <Stack spacing={2} direction={"row"}>
                 <Stack spacing={2} justifyContent="flex-start" direction="column" alignItems="flex-start">
+                    <TextField
+                        label="Film ID"
+                        variant="standard"
+                        value={input.film_id}
+                        onChange={(e) => handleInputChanged("film_id", e.target.value)}
+                    />
+
                     <TextField
                         label="Titel"
                         variant="standard"
@@ -48,6 +72,13 @@ const FilmPage = () => {
                         onChange={(e) =>
                             handleInputChanged("title", e.target.value)
                         }
+                    />
+
+                    <TextField
+                        label="Beschreibung"
+                        variant="standard"
+                        value={input.description}
+                        onChange={(e) => handleInputChanged("description", e.target.value)}
                     />
 
                     <TextField
@@ -59,6 +90,50 @@ const FilmPage = () => {
                                 handleInputChanged("length", e.target.value)
                         }}
                     />
+
+
+                <TextField
+                    label="Release Jahr"
+                    variant="standard"
+                    value={input.release_year}
+                    onChange={(e) => handleInputChanged("release_year", e.target.value)}
+                />
+
+                <TextField
+                    label="Rental Duration"
+                    variant="standard"
+                    value={input.rental_duration}
+                    onChange={(e) => handleInputChanged("rental_duration", e.target.value)}
+                />
+
+                <TextField
+                    label="Rental Rate"
+                    variant="standard"
+                    value={input.rental_rate}
+                    onChange={(e) => handleInputChanged("rental_rate", e.target.value)}
+                />
+
+                <TextField
+                    label="Replacement Cost"
+                    variant="standard"
+                    value={input.replacement_cost}
+                    onChange={(e) => handleInputChanged("replacement_cost", e.target.value)}
+                />
+
+                <TextField
+                    label="Rating"
+                    variant="standard"
+                    value={input.rating}
+                    onChange={(e) => handleInputChanged("rating", e.target.value)}
+                />
+
+                <TextField
+                    label="Special Features"
+                    variant="standard"
+                    value={input.special_features}
+                    onChange={(e) => handleInputChanged("special_features", e.target.value)}
+                />
+
                     <Button variant="contained" onClick={handleSaveClicked}> Save</Button>
                 </Stack>
                 <JsonView value={input}/>
