7c60820865276819d9dd4424677453a92e198013
perdenis <perdomodenis@outlook.com>
2025-05-17 09:34:12 +0200

    Film Page Komponente

--- frontend/src/pages/FilmPage.tsx
+++ frontend/src/pages/FilmPage.tsx
@@ -6,13 +6,30 @@ import Button from "@mui/material/Button";
 import JsonView from "@uiw/react-json-view";
 
 export interface InputType {
+    film_id: string;
     title: string;
+    description: string;
+    release_year: string;
+    rental_duration: string;
+    rental_rate: string;
     length: string;
+    replacement_cost: string;
+    rating: string;
+    special_features: string;
 }
 
 const defaultInput: InputType = {
+    film_id: "0",
     title: "",
-    length: "0"
+    description: "",
+    release_year: "0",
+    rental_duration: "0",
+    rental_rate: "0",
+    length: "0",
+    replacement_cost: "0",
+    rating: "0",
+    special_features: ""
+
 }
 
 const FilmPage = () => {
@@ -51,6 +68,45 @@ const FilmPage = () => {
                     />
 
                     <TextField
+                        label="Beschreibung"
+                        variant="standard"
+                        value={input.description}
+                        onChange={(e) =>
+                            handleInputChanged("description", e.target.value)
+                        }
+                    />
+
+                    <TextField
+                        label={"Jahr der Veröffentlichung"}
+                        variant="standard"
+                        value={input.release_year}
+                        onChange={(e) => {
+                            if(!isNaN(Number(e.target.value)))
+                                handleInputChanged("release_year", e.target.value)
+                        }}
+                    />
+
+                    <TextField
+                        label={"Mietdauer (in Tagen)"}
+                        variant="standard"
+                        value={input.rental_duration}
+                        onChange={(e) => {
+                            if(!isNaN(Number(e.target.value)))
+                                handleInputChanged("rental_duration", e.target.value)
+                        }}
+                    />
+
+                    <TextField
+                        label={"Mietrate (in CHF)"}
+                        variant="standard"
+                        value={input.rental_rate}
+                        onChange={(e) => {
+                            if(!isNaN(Number(e.target.value)))
+                                handleInputChanged("rental_rate", e.target.value)
+                        }}
+                    />
+
+                    <TextField
                         label={"Länge (in Minuten)"}
                         variant="standard"
                         value={input.length}
@@ -59,6 +115,36 @@ const FilmPage = () => {
                                 handleInputChanged("length", e.target.value)
                         }}
                     />
+
+                    <TextField
+                        label={"Ersetzkosten (in CHF)"}
+                        variant="standard"
+                        value={input.replacement_cost}
+                        onChange={(e) => {
+                            if(!isNaN(Number(e.target.value)))
+                                handleInputChanged("replacement_cost", e.target.value)
+                        }}
+                    />
+
+                    <TextField
+                        label={"Bewertung"}
+                        variant="standard"
+                        value={input.rating}
+                        onChange={(e) => {
+                            if(!isNaN(Number(e.target.value)))
+                                handleInputChanged("rating", e.target.value)
+                        }}
+                    />
+
+                    <TextField
+                        label="Spezielle Features"
+                        variant="standard"
+                        value={input.special_features}
+                        onChange={(e) =>
+                            handleInputChanged("special_features", e.target.value)
+                        }
+                    />
+
                     <Button variant="contained" onClick={handleSaveClicked}> Save</Button>
                 </Stack>
                 <JsonView value={input}/>
